4490aa5ab875751cc8d780d044bac588e8f0fac3
lumin <lumin7135@proton.me>
2024-04-18 18:24:22 +0800

    Simulate action when push a broken version to main branch

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/version', (req, res) => {
 
 // For health cehck
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  ';
   res.send('ok');
 });
 
