b20f6b88f338a384a66df2a2c09dac1c37f66536
lumin <lumin7135@proton.me>
2024-04-20 10:45:06 +0800

    upload error test

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static('dist'));
 
+// eslint-disable-next-line no-constant-condition
+if (true) throw new Error('error');
+
 app.get('/version', (req, res) => {
   res.send('2'); // change this string to ensure a new version deployed
 });
 
 // health cehck endpoint
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error('error');
   res.send('ok');
 });
 
