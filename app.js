86f026ad93e2f2b52232476d21bcad1b5e640762
lumin <lumin7135@proton.me>
2024-04-20 10:19:49 +0800

    test action on failed

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.get('/version', (req, res) => {
 
 // health cehck endpoint
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error';
   res.send('ok');
 });
 
